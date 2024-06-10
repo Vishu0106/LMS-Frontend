815304f3cc2435c7594d6fb3e21d26f93b7eaf24
Vishu0106 <kattameedivishnu2003@gmail.com>
2024-06-10 23:30:26 +0530

    Refactor axiosInstance.js to update base URL to production environment

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:8000/api/v1'
+const BASE_URL = 'https://lms-backend-project.onrender.com/api/v1'
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
