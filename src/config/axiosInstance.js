abe4c6dbef77f22db0059c79a87b02cf2815a799
Vishu0106 <kattameedivishnu2003@gmail.com>
2024-07-23 11:26:25 +0530

    Backend api is changed

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'https://lms-backend-project-1.onrender.com/api/v1'
+const BASE_URL = 'http://lms-backend-env.eba-xzmqkxr5.ap-south-1.elasticbeanstalk.com/api/v1'
 const axiosInstance = axios.create();
 
 axiosInstance.defaults.baseURL = BASE_URL;
